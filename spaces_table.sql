4a4009b10eb5ec8152c08ff5add848c54cce14d1
Malika Oubella <moubella@gmail.com>
2022-10-31 15:55:14 +0000

    db setup

---
+++ spaces_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE spaces (
+  id SERIAL PRIMARY KEY,
+  name text,
+  description text,
+  price float,
+  availability text
+);
---
+++ spec/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO spaces (name, description, price, availability) VALUES
+('Riverview Lodge', 'Situated in the centre of Myrtle Beach, Fabulous ocean front penthouse condo has an outdoor swimming pool', 128, '12/12/2022'),
+('Sandestin Resort Studio', 'The apartment has 1 bedroom, 1 bathroom, bed linen, towels, a flat-screen TV with cable channels', 285, '13/12/2022'),
+('Madalena Hermitage', 'Located in the centre of Lisbon, Madalena Hermitage is an 18th-century building offering modern, design apartments.', 175, '13/12/2022'),
+('The Roman Residence', 'The Roman Residence offers free WiFi, air conditioning and household amenities', 275, '14/12/2022');
