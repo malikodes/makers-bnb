f4c6ebcdac6bf42260f437351c65e2ee0a2803d2
Gavin Moorcroft <gavin.moorcroft1@gmail.com>
2022-11-01 10:02:21 +0000

    add ability to add a space

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -18,17 +18,37 @@ RSpec.describe Application do
         reset_spaces_table
     end
 
-    context 'GET /' do
+    context 'GET /spaces' do
         it 'returns a page with all spaces' do
-        response = get('/')
+        response = get('/spaces')
         expect(response.status).to eq 200
         expect(response.body).to include('<h1>All Spaces</h1>')
         end
 
     end
 
+    context 'get /spaces/new' do
+        it 'should add a new space' do
 
+            response_post = post('/spaces/new',name: 'Place 4',
+                description: 'This is place 4',
+                price: 150.00,
+                availability: '01/01/2022')
+     
+            response = get('/spaces')
+    
+            expect(response.status).to eq(200)
+            expect(response.body).to include('<a href="/spaces/4">Place 4')
+        end
 
+        it 'should return a html page with this new space' do
+            response = get('/spaces/4')
+            expect(response.body).to include('<h1>This is place 4</h1>')
+            expect(response.body).to include('<p>This is place 4')
+            expect(response.body).to include('£150')
+            expect(response.body).to include('<p>Availability: 01/01/2022</p>')
+        end
+    end
 end
       
     
